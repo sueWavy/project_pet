ab779cb6dcf783eb7b16e15852844fd1c3ea3606
sueWavy <sueboi0311@gmail.com>
2024-03-08 20:22:15 +0900

    Add : routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
+import Header from "./components/ui/Header";
+import Footer from "./components/ui/Footer";
 
 function App() {
   return (
     <>
-      <div>hi</div>
+      <Header />
+      <Outlet />
+      <Footer />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import NotFound from "./pages/NotFound.tsx";
+import Login from "./pages/Login.tsx";
+import MyPage from "./pages/MyPage.tsx";
+import Write from "./pages/Write.tsx";
+import View from "./pages/View.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/login", element: <Login /> },
+      { path: "/mypage/:id", element: <MyPage /> },
+      { path: "/view/:id", element: <View /> },
+      { path: "/write", element: <Write /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
